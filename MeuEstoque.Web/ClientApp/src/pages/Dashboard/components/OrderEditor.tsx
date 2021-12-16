cba5e2cb294800691223841c5637f65bdd884d16
Wesley Santos <santoswesley1998@gmail.com>
2021-12-16 20:16:47 -0300

    Better order editor

--- MeuEstoque.Web/ClientApp/src/pages/Dashboard/components/OrderEditor.tsx
+++ MeuEstoque.Web/ClientApp/src/pages/Dashboard/components/OrderEditor.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { CreateOrderDTO, getProducts, Order, Product } from "~/api";
-import { Async, AsyncResult } from "~/components/Async";
+import { useAsync } from "~/components/Async";
 import { useSelector } from "~/store";
 
 export interface OrderEditorProps {
@@ -10,6 +10,14 @@ export interface OrderEditorProps {
 
 export function OrderEditor(props: OrderEditorProps) {
   const user = useSelector((state) => state.user);
+  const products = useAsync((signal) => getProducts(signal));
+  const [price, setPrice] = React.useState(props.defaultOrder?.price ?? 1);
+
+  React.useEffect(() => {
+    if (products.state == "complete" && products.value.length > 0) {
+      setPrice(products.value[0].price);
+    }
+  }, [products]);
 
   async function onSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
@@ -36,28 +44,30 @@ export function OrderEditor(props: OrderEditorProps) {
     <form className="container" onSubmit={onSubmit} autoComplete="off">
       <div className="mb-3">
         <label className="form-label">{"Product"}</label>
-        <select name="product-id" className="form-select" required>
-          <Async func={(signal) => getProducts(signal)}>
-            {(result: AsyncResult<Product[], any>) => {
-              switch (result.state) {
-                case "complete": {
-                  return (
-                    <React.Fragment>
-                      {result.value.map((product) => (
-                        <option key={product.id} value={product.id}>
-                          {product.name}
-                        </option>
-                      ))}
-                    </React.Fragment>
-                  );
-                }
-
-                default: {
-                  return <></>;
-                }
-              }
-            }}
-          </Async>
+        <select
+          name="product-id"
+          className="form-select"
+          required
+          disabled={products.state != "complete"}
+          onChange={(e) => {
+            const id = e.currentTarget.value;
+            if (products.state == "complete") {
+              const product = products.value.find(
+                (product) => product.id == id
+              );
+              setPrice(product?.price ?? price);
+            }
+          }}
+        >
+          {products.state == "complete" && (
+            <React.Fragment>
+              {products.value.map((product) => (
+                <option key={product.id} value={product.id}>
+                  {`${product.id} - ${product.name}`}
+                </option>
+              ))}
+            </React.Fragment>
+          )}
         </select>
       </div>
       <div className="mb-3 row">
@@ -71,7 +81,8 @@ export function OrderEditor(props: OrderEditorProps) {
               className="form-control"
               step="0.1"
               min="0"
-              defaultValue={props.defaultOrder?.price ?? 1}
+              onChange={(e) => setPrice(e.currentTarget.valueAsNumber)}
+              value={price}
               required
             />
           </div>
